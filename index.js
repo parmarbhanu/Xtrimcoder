006febc13bb19598841f7da7f2187ba3da72e416
sharma39vishal <sharma39vishal@gmail.com>
2022-12-25 10:42:48 +0530

    Deployment updates

--- index.js
+++ index.js
@@ -38,3 +38,9 @@ app.use("/dis", require("./routers/discussionRouter"));
 // app.get("*",(req,res)=>{
 //     res.sendFile(path.resolve('build','index.html'));
 // })
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require("path");
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
