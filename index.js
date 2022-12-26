144bea8f5d97375029e33fa367fefaac60c9422b
sharma39vishal <sharma39vishal@gmail.com>
2022-12-26 22:14:39 +0530

    remove production

--- index.js
+++ index.js
@@ -34,10 +34,10 @@ app.use("/ques", require("./routers/questionRouter"));
 app.use("/dis", require("./routers/discussionRouter"));
 
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
   const path=require("path");
   app.get('*', (req,res) => {
     res.sendFile(path.resolve( 'client', 'build','index.html'));
 })
-}
+// }
